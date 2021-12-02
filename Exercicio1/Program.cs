245aeaf6d6e145e00bc19b450028bc50db773a0d
Quiel <Quiel@DESKTOP-QPM4FGN>
2021-12-02 06:48:23 -0300

    Add project files.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Entre com as medidas do triângulo X:");
+                double ladoA = float.Parse(Console.ReadLine());
+                double ladoB = float.Parse(Console.ReadLine());
+                double ladoC = float.Parse(Console.ReadLine());
+
+                Triangulo triangulo = new Triangulo(ladoA, ladoB, ladoC);
+
+                Console.WriteLine($"Área do Triângulo: {triangulo.calcularArea().ToString("F4")}");
+                Console.WriteLine("Pressione qualquer tecla para sair");
+
+                Console.ReadKey();
+            }
+            catch (Exception falha)
+            {
+                Console.WriteLine($">{falha.Message}");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Exercicio1/Triangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    public class Triangulo
+    {
+        public double ladoA { get; set; }
+        public double ladoB { get; set; }
+        public double ladoC { get; set; }
+
+        public Triangulo(double ladoA, double ladoB, double ladoC)
+        {
+            this.ladoA = ladoA;
+            this.ladoB = ladoB;
+            this.ladoC = ladoC;
+        }
+
+        public double calcularArea()
+        {
+            double p = (ladoA + ladoB + ladoC) / 2;
+            double area = Math.Sqrt(p * (p - ladoA) * (p - ladoB) * (p - ladoC));
+            return area;
+        }
+    }
+}
